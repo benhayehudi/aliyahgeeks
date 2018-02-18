09024f18c1300d044efc5a2fecbc25f525107e59
benhayehudi <bengreenberg@gmail.com>
2018-02-18 10:50:10 -0500

    introduced navbar component and basic css styling

--- app/javascript/home/components/Home.jsx
+++ app/javascript/home/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../header/components/Header';
+import Navbar from '../../navbar/components/Navbar';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Navbar />
         <div className="body-container">
           CONTENT GOES HERE
         </div>
---
+++ app/javascript/navbar/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar-container">
+        <div className="navbar">
+          <h1>navbar</h1>
+        </div>
+      </div>
+    )
+  }
+}
+export default Navbar
