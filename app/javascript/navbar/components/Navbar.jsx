c2bf8369cda8044221292413ec0b62333939235f
benhayehudi <bengreenberg@gmail.com>
2018-02-20 17:29:08 -0500

    created styling for navbar bookmarks link

--- app/javascript/navbar/components/Navbar.jsx
+++ app/javascript/navbar/components/Navbar.jsx
@@ -8,8 +8,9 @@ class Navbar extends React.Component {
           <div id="navbar-profile-img"><img src="/assets/default-author-headshot.png" alt="default headshot" /></div>
           <div id="navbar-profile-info">Ploni Almoni<br />@plonialmoni</div>
         </div>
-        <div className="navbar-item">
-          <h1>navbar item</h1>
+        <div className="navbar-item navbar-bookmark">
+          <div id="navbar-bookmark-img"><img src="/assets/icons/bookmark-icon.png" alt="bookmark icon" /></div>
+          <div id="navbar-profile-info">My Bookmarks</div>
         </div>
         <div className="navbar-item">
           <h1>navbar item</h1>
