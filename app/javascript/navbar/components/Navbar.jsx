7902f9ec374892da7b50ccfaafdaedf62cbe69ad
benhayehudi <bengreenberg@gmail.com>
2018-02-21 17:41:38 -0500

    added sponsors and links navbar elements

--- app/javascript/navbar/components/Navbar.jsx
+++ app/javascript/navbar/components/Navbar.jsx
@@ -12,11 +12,22 @@ class Navbar extends React.Component {
           <div id="navbar-bookmark-img"><img src="/assets/icons/bookmark-icon.png" alt="bookmark icon" /></div>
           <div id="navbar-profile-info">My Bookmarks</div>
         </div>
-        <div className="navbar-item">
-          <h1>navbar item</h1>
+        <div className="navbar-item navbar-sponsors">
+          <span id="sponsors-heading"><span id="heart">&hearts;</span>Sponsors<span id="heart">&hearts;</span></span>
+          <div id="navbar-sponsor-img"><img src="/assets/sponsors-wanted.png" alt="sponsor wanted" /></div>
+          <div id="navbar-sponsor-lead">
+            Want to support a new initiative for the English speaking Aliyah community? Looking to reach an Anglo audience interested in all things Israel? Consider becoming a sponsor of Rechov Aliyah today!
+          </div>
         </div>
-        <div className="navbar-item">
-          <h1>navbar item</h1>
+        <div className="navbar-item navbar-links">
+          <span id="links-heading">Get Around</span>
+          <span id="link-item">About</span>
+          <span id="link-item">Sponsors</span>
+          <span id="link-item">Shop Rechov Aliyah</span>
+          <span id="link-item">Privacy Policy</span>
+          <span id="link-item">Terms of Use</span>
+          <span id="link-item">Code of Conduct</span>
+          <span id="link-item">Contact</span>
         </div>
       </div>
     )
