1ccd3ff337fcb855855dfb509d042ac501015add
benhayehudi <bengreenberg@gmail.com>
2018-03-22 17:22:04 -0400

    adjusted css for loading box

--- app/javascript/components/containers/LoadingBox.jsx
+++ app/javascript/components/containers/LoadingBox.jsx
@@ -8,8 +8,8 @@ class LoadingBox extends React.Component {
         <div className="edit-profile-box">
           <div id="title-firsthalf" className="loading-box">Nu?? Savlanut!</div>
           <div id="post-body" className="loading-box">
-            <img src="/assets/loading-box-img.jpg" className="loading-box"/><br/>
-            Your content is being loaded from our top secret super high tech servers. If you are seeing this for too long then chances are there is no content here. If that's the case, see that blue button the top of this page? Yes, that's for you... go ahead, make some content and share your story!
+            <div><img src="/assets/loading-box-img.jpg" className="loading-box"/></div><br/>
+            <div>Your content is being loaded from our top secret super high tech servers. If you are seeing this for too long then chances are there is no content here. If that's the case, see that blue button the top of this page? Yes, that's for you... go ahead, make some content and share your story!</div>
           </div>
           <div id="login-footer">
             Don't have an account yet? <a href="/sessions/new">Sign up today!</a>
