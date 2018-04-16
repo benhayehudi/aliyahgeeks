a5eaca41111fb895ce2614079ceebda057bb7c79
benhayehudi <bengreenberg@gmail.com>
2018-04-16 17:28:10 -0400

    added tags and tags join tables

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 201802225183660) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "postlikes", force: :cascade do |t|
     t.integer "post_id"
     t.integer "likes", default: 0
@@ -55,6 +62,12 @@ ActiveRecord::Schema.define(version: 201802225183660) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
